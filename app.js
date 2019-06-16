7456e9bf75163e3acfdef4efb14dab475939e50b
Zoilo Granda <zgranda@gmail.com>
2019-06-16 14:52:52 -0400

    Separated execution on different functions Improved error handling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express()
+const app = express();
 const http = require('http').Server(app)
 const port = 8080;
 const https = require('https');
@@ -24,7 +24,7 @@ async function processHandler() {
       console.log('error');
       console.log('e.message:',e.message);
       console.log(e);
-      if (e.message === 'net::ERR_CONNECTION_TIMED_OUT at https://192.168.1.1') {
+      if (e.message.includes('ERR_CONNECTION_TIMED_OUT')||e.message.includes('ERR_CONNECTION_REFUSED')) {
         console.log('connection error');
         await connectoToWifi();
       }if (e.message.includes('textContent')) {
@@ -33,12 +33,6 @@ async function processHandler() {
       }
     } 
   } while (!weHaveInternet);
-}
-
-function logCurrentTime() {
-  var date = new Date()
-  var time = date.getHours() +':'+ date.getMinutes() +':' + date.getSeconds()
-  console.log(time);
 }
 
 async function startProcess() {
@@ -51,72 +45,18 @@ async function startProcess() {
     ignoreHTTPSErrors: true
   });
   const page = await browser.newPage()
-  await page.waitFor(15000)
-  await page.goto('https://192.168.1.1');
-  await page.focus('#txt_Username');
-  await page.keyboard.type(environment_data.login_username);
-  await page.focus('#txt_Password');
-  await page.keyboard.type(environment_data.login_password);
-  await page.click('#btnLogin');
-  console.log('logged in');
-  await page.waitFor("#listfrm");
-  const wanClick = await page.evaluate(()=>{
-    var iframe = document.querySelector("#listfrm")
-    var wanButton = iframe.contentDocument.querySelector("#link_Admin_0_1")
-    console.log(iframe);
-    console.log(wanButton);
-    wanButton.click();
-    return wanButton;
-  });
-  console.log('wan clicked');
-  await page.waitFor("#contentfrm")
-  const hasInternet = await page.evaluate(()=>{
-    var iframe = document.querySelector("#contentfrm");
-    console.log(iframe);
-    var textElement = iframe.contentDocument.querySelector("#Estado\\ de\\ la\\ conexión > table > tbody > tr:nth-child(4) > td:nth-child(2)").textContent
-    var conectadoCheck = textElement ==="Conectado "? true : false;
-    console.log({textElement});
-    console.log({conectadoCheck});
-    return conectadoCheck;
-  });
+  var doLoginVar = await doLogin(page);
+  var clickOnWANVar = await clickOnWAN(page);
+  var hasInternet = await checkConectadoStatus(page);
+  console.log('Tienes internet: ',hasInternet);
   if (hasInternet) {
     console.log('YA TIENES INTERNET');
     await browser.close();
     process.exit();
-  } else {
-    console.log('NO TIENES INTERNET');
-    const mantenimientoClick = await page.evaluate(()=>{
-      var iframe = document.querySelector("#listfrm")
-      var mantenimientoButton = iframe.contentDocument.querySelector("#link_Admin_3")
-      console.log(iframe);
-      console.log(mantenimientoButton);
-      mantenimientoButton.click();
-      return mantenimientoButton;
-    });
   }
-  console.log('mantenimiento clicked');
-  const dispositivoClick = await page.evaluate(()=>{
-    var iframe = document.querySelector("#listfrm")
-    var dispositivoButton = iframe.contentDocument.querySelector("#link_Admin_3_1")
-    console.log(iframe);
-    console.log(dispositivoButton);
-    dispositivoButton.click();
-    return dispositivoButton;
-  });
-  console.log('dispositivo clicked');
-  page.on('dialog', (dialog)=> {
-    console.log('dialog detected');
-    dialog.accept();
-  });
-  const resetClick = await page.evaluate(()=>{
-    var iframe = document.querySelector("#contentfrm")
-    var resetButton = iframe.contentDocument.querySelector("body > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > button")
-    console.log(iframe);
-    console.log(resetButton);
-    resetButton.click();
-    return resetButton;
-  });
-  console.log('reset clicked');
+  var clickOnMantenimientoVar = await clickOnMantenimiento(page);
+  var clickOnDispositivoVar = await clickOnDispositivo(page)
+  var clickOnResetVar = await clickOnReset(page)
   logCurrentTime();
   await page.waitFor(60000 * 5); //minutes
   logCurrentTime();
@@ -125,6 +65,88 @@ async function startProcess() {
   logCurrentTime();
   await browser.close();
 };
+
+async function doLogin(page){
+  await page.waitFor(6000)
+  await page.goto('https://192.168.1.1');
+  await page.focus('#txt_Username');
+  await page.keyboard.type(environment_data.login_username);
+  await page.focus('#txt_Password');
+  await page.keyboard.type(environment_data.login_password);
+  await page.click('#btnLogin');
+  console.log('logged in');
+  return true;
+}
+
+async function clickOnWAN(page) {
+  await page.waitFor("#listfrm");
+  console.log('wan clicking');
+  return await page.evaluate(()=>{
+    var iframe = document.querySelector("#listfrm")
+    var wanButton = iframe.contentDocument.querySelector("#link_Admin_0_1")
+    console.log(iframe);
+    console.log(wanButton);
+    wanButton.click();
+    return true;
+  });
+}
+
+async function checkConectadoStatus(page) {
+  await page.waitFor("#contentfrm");
+  return await page.evaluate(()=>{
+    var iframe = document.querySelector("#contentfrm");
+    console.log(iframe);
+    var textElement = iframe.contentDocument.querySelector("#Estado\\ de\\ la\\ conexión > table > tbody > tr:nth-child(4) > td:nth-child(2)").textContent
+    var conectadoCheck = textElement ==="Conectado "? true : false;
+    return conectadoCheck;
+  });
+}
+
+async function clickOnMantenimiento(page) {
+  console.log('NO TIENES INTERNET');
+  console.log('mantenimiento clicking');
+  return await page.evaluate(()=>{
+    var iframe = document.querySelector("#listfrm")
+    var mantenimientoButton = iframe.contentDocument.querySelector("#link_Admin_3")
+    console.log(iframe);
+    console.log(mantenimientoButton);
+    mantenimientoButton.click();
+    return true;
+  });
+}
+
+async function clickOnDispositivo(page){
+  console.log('dispositivo clicking');
+  return await page.evaluate(()=>{
+    var iframe = document.querySelector("#listfrm")
+    var dispositivoButton = iframe.contentDocument.querySelector("#link_Admin_3_1")
+    console.log(iframe);
+    console.log(dispositivoButton);
+    dispositivoButton.click();
+    return true;
+  });
+}
+async function clickOnReset(page){
+  console.log('reset clicking');
+  page.on('dialog', (dialog)=> {
+    console.log('dialog detected');
+    dialog.accept();
+  });
+  return await page.evaluate(()=>{
+    var iframe = document.querySelector("#contentfrm")
+    var resetButton = iframe.contentDocument.querySelector("body > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > button")
+    console.log(iframe);
+    console.log(resetButton);
+    resetButton.click();
+    return true;
+  });
+}
+
+function logCurrentTime() {
+  var date = new Date()
+  var time = date.getHours() +':'+ date.getMinutes() +':' + date.getSeconds()
+  console.log(time);
+}
 
 async function connectoToWifi() {
   var currentOS = os.platform();
